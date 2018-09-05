47f6f9df7444eb334b3dbb5d97d18bf7d94215e4
Patrick Roughan <patrickroughan@realexpayments.com>
2018-09-05 11:53:51 +0100

    first CI

--- Berksfile
+++ Berksfile
@@ -1,3 +1,4 @@
 source 'https://supermarket.chef.io'
-
+cookbook 'java'
+cookbook 'jenkins'
 metadata
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,12 @@
 #
-# Cookbook:: jenkins_wrapper
+# Cookbook:: pcp_jenkins
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+Chef::Log.debug('Running')
+
+include_recipe 'java::default'
+include_recipe 'jenkins::default'
+
+Chef::Log.debug('Done')
