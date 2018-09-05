1230e3a5f8acd5379586a0b71b3a48dd7c8a15a9
Patrick Roughan <patrickroughan@realexpayments.com>
2018-09-05 15:40:32 +0100

    7

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,19 +8,4 @@ Chef::Log.debug('Running')
 
 include_recipe 'java'
 
-name "java"
-description "Install Oracle Java"
-default_attributes(
-  "java" => {
-    "install_flavor" => "oracle",
-    "jdk_version" => "8",
-    "oracle" => {
-      "accept_oracle_download_terms" => true
-    }
-  }
-)
-run_list(
-  "recipe[java]"
-)
-
 Chef::Log.debug('Done')
