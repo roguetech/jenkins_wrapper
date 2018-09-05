494bf8af52c64f65f0d2f1b4f03168c0c4057473
Patrick Roughan <patrickroughan@realexpayments.com>
2018-09-05 14:15:42 +0100

    4

--- Berksfile
+++ Berksfile
@@ -2,4 +2,5 @@ source 'https://supermarket.chef.io'
 cookbook 'java',
 #cookbook 'jenkins',
 cookbook 'jenkins_wrapper',
+cookbook 'windows', '~> 5.1.0'
 metadata
--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,7 @@ long_description 'Installs/Configures pcp_jenkins'
 version '0.1.0'
 depends 'java', '2.2.0'
 depends 'jenkins', '6.2.0'
+depends 'windows', '~> 5.1.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
